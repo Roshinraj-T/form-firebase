1ef8cc6959a5783fe8ee39550d1ac8b7776f52c8
Roshinraj <roshinraj.thangasamy@finstein.ai>
2023-10-16 13:11:19 +0530

    insert and retrieve data from firestore

--- script.js
+++ script.js
@@ -1,54 +1,69 @@
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        const firebaseConfig = {
-            apiKey: "AIzaSyBr_4N-kDvEUD6H6_czJpV3_WQERYOwXwE",
-            authDomain: "notification-a1dc8.firebaseapp.com",
-            databaseURL: "https://notification-a1dc8-default-rtdb.firebaseio.com",
-            projectId: "notification-a1dc8",
-            storageBucket: "notification-a1dc8.appspot.com",
-            messagingSenderId: "950550976003",
-            appId: "1:950550976003:web:e4194b60c44f96ef29e036",
-            measurementId: "G-VFX2C1VEN4"
-        };
-        let html=``
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBr_4N-kDvEUD6H6_czJpV3_WQERYOwXwE",
+    authDomain: "notification-a1dc8.firebaseapp.com",
+    databaseURL: "https://notification-a1dc8-default-rtdb.firebaseio.com",
+    projectId: "notification-a1dc8",
+    storageBucket: "notification-a1dc8.appspot.com",
+    messagingSenderId: "950550976003",
+    appId: "1:950550976003:web:e4194b60c44f96ef29e036",
+    measurementId: "G-VFX2C1VEN4"
+};
 
-        // Initialize Firebase
-        const app = firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+const firestore = app.firestore();
 
-        // Reference to your database
-        var database = firebase.database();
 
-        // Read data from the database
-        database.ref('user').once('value', function (snapshot) {
-            var data=snapshot.val();
-            if (data) {
-       // Iterate over the keys in the data
-       for (var key in data) {
-        if (data.hasOwnProperty(key)) {
-          var item = data[key];
-          console.log(item);
-          html+=
-            `<tr>
-                <td>${item.email}</td>
-                <td>${item.name}</td> 
-                <td>${item.number}</td>
-            </tr>`
-      }
+let html = ``;
+fetchUsers();
+
+async function fetchUsers() {
+    try {
+        const querySnapshot = await firestore.collection('users').get();
+
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            html +=
+                `<tr>
+                            <td>${data.name}</td>
+                            <td>${data.email}</td>
+                            <td>${data.number}</td>
+
+                        </tr>`;
+        });
+
+        document.getElementById('data').innerHTML = html;
+    } catch (error) {
+        console.error('Error getting documents: ', error);
     }
-  }
-  document.getElementById('data').innerHTML=html
-
-});
-function insertData() {
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let number = document.getElementById("number").value;
-
-    database.ref('user').push(
-        {
-            name: name,
-            email: email,
-            number:number
-        }
-    );
 }
+
+async function addUserData(data) {
+    try {
+        // Get a reference to the 'users' collection
+        const collectionRef = firestore.collection('users');
+
+        // Add the provided data to the collection
+        const docRef = await collectionRef.add(data);
+        html = ``
+        fetchUsers();
+
+        console.log('Document added with ID:', docRef.id);
+    } catch (error) {
+        console.error('Error adding document:', error);
+    }
+}
+// Add an event listener to the button
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('addDataButton').addEventListener('click', () => {
+        const data = {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            number: document.getElementById('number').value,
+        };
+
+        // Call the function to add the data
+        addUserData(data);
+    });
+})
