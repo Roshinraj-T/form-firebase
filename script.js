1c56c7e03d10e146eb3a98f4a3665051bef02460
Roshinraj-T <87963299+Roshinraj-T@users.noreply.github.com>
2024-12-21 22:45:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Initialize Firebase with your configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAbn72uIcgzF3ejFctXLcRiSdled47JSns",
-    authDomain: "fir-3037c.firebaseapp.com",
-    databaseURL: "https://fir-3037c-default-rtdb.firebaseio.com",
-    projectId: "fir-3037c",
-    storageBucket: "fir-3037c.appspot.com",
-    messagingSenderId: "664847263337",
-    appId: "1:664847263337:web:9c7e0c3e7f067d6585e4f8",
-    measurementId: "G-X8BK253KL1"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
