b00491eaf94f6465d71c56926be3babc0075f9fb
Roshinraj-T <87963299+Roshinraj-T@users.noreply.github.com>
2024-12-21 22:46:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
         // Your web app's Firebase configuration
         // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        const firebaseConfig = {
-            apiKey: "AIzaSyAbn72uIcgzF3ejFctXLcRiSdled47JSns",
-            authDomain: "fir-3037c.firebaseapp.com",
-            databaseURL: "https://fir-3037c-default-rtdb.firebaseio.com",
-            projectId: "fir-3037c",
-            storageBucket: "fir-3037c.appspot.com",
-            messagingSenderId: "664847263337",
-            appId: "1:664847263337:web:9c7e0c3e7f067d6585e4f8",
-            measurementId: "G-X8BK253KL1"
-          };
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
         let html=``
 
         // Initialize Firebase
